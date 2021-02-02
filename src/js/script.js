a194c61bd037170cdc559d40aa3f828e8af3ad64
Shashini Probodha <shashiniprabodha0503@gmail.com>
2021-02-02 23:41:10 +0530

    v.2.1.0 - skill section update

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,34 @@ $(document).ready(function () {
     });
 
 
+    let options = {
+        startAngle: -1.55,
+        size: 126,
+        value: 0.85,
+        fill: {gradient: ['#0ff5ef', '#ac0276', "#f634e4"]}
+    }
+    $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+        function(event, progress, stepValue){
+            $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+        });
+    $(".java .bar").circleProgress({
+        value: 0.70
+    });
+    $(".html .bar").circleProgress({
+        value: 0.65
+    });
+    $(".sql .bar").circleProgress({
+        value: 0.50
+    });
+    $(".js .bar").circleProgress({
+        value: 0.20
+    });
+    $(".hb .bar").circleProgress({
+        value: 0.55
+    });
+    $(".spring .bar").circleProgress({
+        value: 0.00
+    });
+
+
 });
